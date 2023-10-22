a06e012ae4189d86cd725c39cbdc82d89e8739ae
Sidath Samarasinghe <sidaath@gmail.com>
2023-10-22 16:35:22 +0530

    with hashmap

--- merge-sorted-array/MergeSortedArray.java
+++ merge-sorted-array/MergeSortedArray.java
@@ -1,55 +1,39 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int pointerNum1 = m - 1;
-        int pointerNum2 = n - 1;
-        int finalPlace = m + n -1;
+        Map<Integer, Integer> hashMap = new TreeMap<Integer, Integer>();
 
-        if (n == 0){
-            finalPlace = -1; 
-        }else if(m == 0){
-            for(int i = 0; i < n; i++){
-                nums1[i] = nums2[i];
+        for(int i = 0; i < m; i ++){
+            System.out.println("Int i = "+i);
+            if(hashMap.containsKey(nums1[i])){
+                hashMap.put(nums1[i], (hashMap.get(nums1[i]) + 1));
+            }else{
+                hashMap.put(nums1[i], 1);
             }
-            finalPlace = -1;
         }
 
-        while (finalPlace >= 0 ){
-
-            if(pointerNum1 < 0 || pointerNum2 < 0){
-                if(pointerNum1 <0){
-                    while(pointerNum2 >= 0){
-                        nums1[finalPlace] = nums2[pointerNum2];
-                        pointerNum2 = pointerNum2 - 1;
-                        finalPlace = finalPlace - 1;
-                    }
-                }else{
-                    while(pointerNum1 >= 0){
-                        nums1[finalPlace] = nums1[pointerNum1];
-                        pointerNum1 = pointerNum1 - 1;
-                        finalPlace = finalPlace - 1; 
-                    }
-                }
+        for (int i = 0; i < n; i++){
+            if(hashMap.containsKey(nums2[i])){
+                hashMap.put(nums2[i], (hashMap.get(nums2[i]) + 1));
+            }else{
+                hashMap.put(nums2[i], 1);
             }
+        }
 
-            if (pointerNum1 >= 0 && pointerNum2 >= 0 && nums1[pointerNum1] > nums2[pointerNum2]){
-                while(pointerNum1 >= 0 && pointerNum2 >= 0 && nums1[pointerNum1] > nums2[pointerNum2]){
-                    nums1[finalPlace] = nums1[pointerNum1];
-                    finalPlace = finalPlace -1;
-                    pointerNum1 = pointerNum1 - 1;
-                }
-            }else if (pointerNum1 >= 0 && pointerNum2 >= 0 && nums1[pointerNum1] < nums2[pointerNum2]){
-                while(pointerNum1 >= 0 && pointerNum2 >= 0 && nums1[pointerNum1] < nums2[pointerNum2]){
-                    nums1[finalPlace] = nums2[pointerNum2];
-                    finalPlace = finalPlace - 1;
-                    pointerNum2 = pointerNum2 - 1;
-                }
-            }else if (pointerNum1 >= 0 && pointerNum2 >= 0 && nums1[pointerNum1] == nums2[pointerNum2]){
-                nums1[finalPlace] = nums1[pointerNum1];
-                nums1[finalPlace-1] = nums2[pointerNum2];
-                finalPlace = finalPlace - 2;
-                pointerNum1 = pointerNum1 -1;
-                pointerNum2 = pointerNum2 -1;
-            }
+        int currentPos = 0;
+
+        int key = 0;
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()){
+            key = entry.getKey();
+            count = entry.getValue();
+            int nextPos = currentPos + count;
+            while(currentPos < nextPos){
+                nums1[currentPos] = key;
+                currentPos = currentPos + 1;
+            }    
         }
     }
 }
