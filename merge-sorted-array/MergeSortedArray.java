90dd43e87d5ff5eec400058ec2d81ff03beea2b3
Sidath Samarasinghe <sidaath@gmail.com>
2023-11-05 22:15:53 +0530

    optimized

--- merge-sorted-array/MergeSortedArray.java
+++ merge-sorted-array/MergeSortedArray.java
@@ -1,39 +1,34 @@
-import java.util.Map;
-import java.util.TreeMap;
-
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        Map<Integer, Integer> hashMap = new TreeMap<Integer, Integer>();
-
-        for(int i = 0; i < m; i ++){
-            System.out.println("Int i = "+i);
-            if(hashMap.containsKey(nums1[i])){
-                hashMap.put(nums1[i], (hashMap.get(nums1[i]) + 1));
-            }else{
-                hashMap.put(nums1[i], 1);
-            }
-        }
-
-        for (int i = 0; i < n; i++){
-            if(hashMap.containsKey(nums2[i])){
-                hashMap.put(nums2[i], (hashMap.get(nums2[i]) + 1));
-            }else{
-                hashMap.put(nums2[i], 1);
-            }
-        }
-
-        int currentPos = 0;
-
-        int key = 0;
+    public int removeElement(int[] nums, int val) {
+        int endArray = nums.length - 1;
+        int beginArray = 0;
         int count = 0;
-        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()){
-            key = entry.getKey();
-            count = entry.getValue();
-            int nextPos = currentPos + count;
-            while(currentPos < nextPos){
-                nums1[currentPos] = key;
-                currentPos = currentPos + 1;
-            }    
+    
+        if(val > 50){
+            return nums.length;
         }
+
+        if(endArray == beginArray){
+            return  nums[0] == val ?  0 : 1;
+        }
+
+        while (endArray >= beginArray){
+            if(nums[beginArray] == val && nums[endArray] != val){
+                nums[beginArray] = nums[endArray];
+                count++;
+                beginArray  = beginArray + 1;
+                endArray    = endArray - 1;
+            }else if(nums[beginArray] == val && nums[endArray] == val){
+                count++;
+                endArray = endArray - 1;
+            }else if (nums[beginArray] != val && nums[endArray] == val){
+                count++;
+                endArray = endArray - 1;
+            }else{
+                beginArray = beginArray + 1;
+            }
+        }
+
+        return nums.length - count;
     }
 }
