062eb1067c286af3d34ff32dc4080cfb56c16376
Sidath Samarasinghe <sidaath@gmail.com>
2023-11-15 21:35:13 +0530

    remove dup 2

---
+++ remove-duplicate-2/RemoveDup2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int nextValidNumberIndex =1;
+        int count = 1;
+
+        for (int i = 1; nextValidNumberIndex < nums.length; i++){
+            nums[i] = nums[nextValidNumberIndex];
+
+            if(nums[i] == nums[i-1]){
+                while(nextValidNumberIndex<nums.length && nums[nextValidNumberIndex] == nums[i-1]){
+                    nextValidNumberIndex = nextValidNumberIndex + 1;
+                }
+            }else{
+                nextValidNumberIndex++;
+            }
+            count++;
+        }
+
+        StringBuilder s = new StringBuilder("[");
+        for(int i =0; i < nums.length; i++){
+            s.append(nums[i]+",");
+        }
+        s.append("]");
+
+        System.out.println(s);
+        System.out.println("return == "+count);
+        return count;
+    }
+}
