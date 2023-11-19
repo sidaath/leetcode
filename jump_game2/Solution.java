cc28272cb4ec4775099b836108f336b2caf2e61b
Sidath Samarasinghe <sidaath@gmail.com>
2023-11-19 21:13:24 +0530

    working solution, beat 23% on speed 71% memory

--- jump_game2/Solution.java
+++ jump_game2/Solution.java
@@ -3,46 +3,46 @@
 public class Solution{
     
     public int jump(int[] nums){
+        int distance            = nums.length - 1;
+        int distanceTravelled   = 0;
 
-        int jumpCount = 0;
-        int maxJump = 0;
-        int curIndex = 0;
-        int prevLoc = 0;
+        int curIndex = nums.length-1;
 
-        int[] options = new int[nums.length];
 
-        for(int i = 0; i < nums.length; i++){
-            options[i] = nums[i];
-        }
+        int count = 0;
+        //lowest number of additions that equal to or greater than distance
+        //subset with smallest length that adds up to or greate than distance
 
-        while (curIndex < nums.length){
-            System.out.println("Current index = "+curIndex);
-            maxJump = options[curIndex];
+        
 
-            if(maxJump == 0){
-                System.out.println("Option 1 in loop");
-                curIndex = prevLoc;
-                options[curIndex] = --options[curIndex];
-                --jumpCount;
-            }else{
-                System.out.println("Option 2 in loop");
-                maxJump = options[curIndex];   
-                if(maxJump > 1){
-                    for(int i = 1; i <= maxJump; i++){
-                        if(curIndex+i < nums.length && curIndex+maxJump < nums.length && options[curIndex+ i] > options[curIndex + maxJump]){
-                            maxJump = i;
-                        }
-                    }
-                }
-                prevLoc = curIndex;
-                curIndex = maxJump + prevLoc;
-                ++jumpCount;
+
+       while(distanceTravelled < distance){
+        System.out.println("Starting while loop");
+        System.out.println("curIndex = "+curIndex);
+        System.out.println("Dis Travlled = "+distanceTravelled);
+        
+
+        int distanceFromPoint = 0;
+        System.out.println("Init distanceFromPoint = "+distanceFromPoint);
+        int curIndexTemp = curIndex;
+        System.out.println("Init curIndexTemp = "+curIndexTemp);
+
+        for(int i = curIndex; i >= 0; i--){
+            System.out.println("i = "+i+" curIndexTemp = "+curIndexTemp+" distanceFromPoint = "+distanceFromPoint);
+            if(curIndex - i > distanceFromPoint && i+nums[i] >= curIndex){
+                System.out.println("changing distance from point and curIndexTemp");
+                distanceFromPoint = curIndex - i;
+                curIndexTemp = i;
+                System.out.println("New vals | curIndexTemp = "+curIndexTemp+" | distanceFromPoint = "+distanceFromPoint);
             }
         }
 
+        distanceTravelled = distanceTravelled + (curIndex - curIndexTemp);
+        curIndex = curIndexTemp;
+        System.out.println("SET NEXT LOOP VALS : curIndex = "+curIndex+" | distanceTravelled = "+distanceTravelled);
+        count++;
+       }
 
-
-
-        return jumpCount;
+       return count;
     }
 }
