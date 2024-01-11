972174ca649b5c939349ca3f86f53fc0c2e5794a
Sidath Samarasinghe <sidaath@gmail.com>
2024-01-11 22:15:42 +0530

    add tests, oreder off

--- hindex/Solution.java
+++ hindex/Solution.java
@@ -1,8 +1,38 @@
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Solution{
+    public static void main(String[] args) {
+        int[] ar1 = {1};
+        int[] ar2 = {1,1,1,1,1,5};
+        int[] ar3 = {5,6,7,8};
+        int[] ar4 = {0,6,1,5,3};
+        int[] ar5 = {5,5,6,7,8};
+        int[] ar6 = {1,0,4,5,2,3};
+        
+        System.out.println("************** START ****************");
+        int res1 = hIndex(ar1);
+        System.out.println("res1 = "+res1);
+        System.out.println("**************************************");
+        int res2 = hIndex(ar2);
+        System.out.println("res2 = "+res2);
+        System.out.println("**************************************");
+        int res3 = hIndex(ar3);
+        System.out.println("res3 = "+res3);
+        System.out.println("**************************************");
+        int res4 = hIndex(ar4);
+        System.out.println("res4 = "+res4);
+        System.out.println("**************************************");
+        int res5 = hIndex(ar5);
+        System.out.println("res5 = "+res5);
+        System.out.println("**************************************");
+        int res6 = hIndex(ar6);
+        System.out.println("res6 = "+res6);
+        System.out.println("**************************************");
+        
+    }
 
-    public int hIndex(int[] citations) {
+    public static int hIndex(int[] citations) {
         //read integer
         //if greater than 0, put to map as key
         //value of map -> +1 for each citation that is greater than key
@@ -18,9 +48,7 @@ public class Solution{
                     //so increment value for that key
                     //increment value for all keys less than or equal to current val
                     for (Integer key : citMap.keySet()) {
-                        System.out.println("pr k = "+key);
                         if (key <= Integer.valueOf(citations[i])){
-                            System.out.println("in if for k "+key);
                             int curVal = citMap.get(key);
                             citMap.put(key, curVal+1);
                         }
@@ -29,9 +57,7 @@ public class Solution{
                     //key absent
                     citMap.put(Integer.valueOf(citations[i]), Integer.valueOf(0));
                     for (Integer key : citMap.keySet()) {
-                        System.out.println("ab k = "+key);
                         if (key <= Integer.valueOf(citations[i])){
-                            System.out.println("in if for k "+key);
                             int curVal = citMap.get(key);
                             citMap.put(key, curVal+1);
                         }
